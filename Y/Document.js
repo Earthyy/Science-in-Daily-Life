646624c316cf3fdff93565f81e365774995815e4
Earthyy <90510021+Earthyy@users.noreply.github.com>
2022-02-08 23:50:11 +0700

    เผื่อ รูป

---
+++ Y/Document.js
@@ -0,0 +1,32 @@
+
+$(function() {
+
+    $(".bars").on("click", function() {
+        if($(".tabC").hasClass("active")) {
+            $(".tabC").removeClass("active");
+          $(this).find(a).html("<i class='fas fa-bars'></i>");
+
+
+        } else{
+            $(".tabC").addClass("active");
+           $(this).find(a).html("<i class='fas fa-times'</i>");
+
+           
+        }
+    });
+   
+});
+
+var tabbar = document.getElementById('tabbar');
+window.onscroll = function(){
+    if(window.pageYOffset > 100) {
+        tabbar.style.position='fixed';
+        tabbar.style.top = 0;
+    }
+    else{
+        tabbar.style.position='fixed';
+        tabbar.style.top = 100;
+    }
+}
+
+
